84fccc363b778e077554c36e35efaf1f669adae2
Praharsh Singh <praharsh535@gmail.com>
2020-05-22 22:19:42 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.site-slider').not('.slick-initialized').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '.site-slider .slider-btn .prev',
+    nextArrow: '.site-slider .slider-btn .next',
+});
